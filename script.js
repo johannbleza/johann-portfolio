d028c97dce2c52c6db3ab021584c124be15a36a2
Johann Sealtiel Bleza <johannscb@Johanns-MacBook-Air.local>
2025-03-03 15:17:29 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,213 @@
+gsap.from([".greeting", ".name-title"], {
+  scrollTrigger: {
+    trigger: ".greeting",
+    toggleActions: "play reverse restart none",
+  },
+  x: -200,
+  duration: 0.5,
+  stagger: 0.1,
+  opacity: 0,
+});
+
+gsap.from([".subtitle", ".resume-button", ".home-svg"], {
+  scrollTrigger: {
+    trigger: ".greeting",
+    toggleActions: "play reverse restart none",
+  },
+  y: 200,
+  duration: 0.5,
+  stagger: 0.1,
+  opacity: 0,
+});
+
+gsap.from(".profile-img", {
+  scrollTrigger: {
+    trigger: ".profile-img",
+    toggleActions: "play reverse restart none",
+  },
+  duration: 1,
+  // opacity: 0,
+  scale: 0.5,
+});
+gsap.from(".about-me-button", {
+  scrollTrigger: {
+    trigger: ".about-me-button",
+    toggleActions: "play reverse restart none",
+  },
+  duration: 1,
+  // opacity: 0,
+  x: -200,
+});
+
+gsap.from(".mouse", {
+  scrollTrigger: {
+    trigger: ".greeting",
+    toggleActions: "play reverse restart none",
+  },
+  y: 300,
+  opacity: 0,
+  scale: 0,
+});
+
+gsap.from(".nav-links", {
+  scrollTrigger: {
+    trigger: ".greeting",
+    toggleActions: "play reverse restart none",
+  },
+  y: -200,
+  opacity: 0,
+  stagger: 0.1,
+});
+
+gsap.from(".skills-title", {
+  scrollTrigger: {
+    trigger: ".skills-title",
+    toggleActions: "restart reverse play reverse",
+  },
+  y: -200,
+  opacity: 0,
+  duration: 0.5,
+});
+
+gsap.utils.toArray(".skill").forEach((skill, i) => {
+  gsap.from(skill, {
+    scrollTrigger: {
+      trigger: "skills-title",
+      // trigger: skill,
+      toggleActions: "restart reverse play reverse",
+    },
+    x: 200,
+    opacity: 0,
+    duration: 0.5,
+    delay: i * 0.1,
+  });
+});
+
+gsap.from(".projects-title", {
+  scrollTrigger: {
+    trigger: ".project-card",
+    toggleActions: "restart reverse play reverse",
+  },
+  y: -200,
+  opacity: 0,
+  duration: 0.5,
+});
+
+gsap.utils.toArray(".project-card").forEach((card, i) => {
+  gsap.from(card, {
+    scrollTrigger: {
+      trigger: card,
+      toggleActions: "restart reverse play reverse",
+    },
+    x: -400,
+    duration: 0.5,
+    delay: i * 0.2,
+    opacity: 0,
+  });
+});
+
+gsap.utils.toArray(".education-block").forEach((block, i) => {
+  gsap.from(block, {
+    scrollTrigger: {
+      trigger: block,
+      toggleActions: "restart reverse play reverse",
+    },
+    opacity: 0,
+    duration: 1,
+    delay: i * 0.2,
+  });
+});
+
+gsap.from(".timeline-line", {
+  scrollTrigger: {
+    trigger: ".education-block",
+    toggleActions: "play none none reverse",
+  },
+  y: -200,
+  scaleY: 0,
+  transformOrigin: "top bottom",
+});
+
+gsap.from(".timeline-circle", {
+  scrollTrigger: {
+    trigger: ".timeline-line",
+    start: "top center",
+    toggleActions: "play none none reverse",
+  },
+  opacity: 0,
+  scale: 0,
+});
+
+gsap.from(".education-title", {
+  scrollTrigger: {
+    trigger: ".education-block",
+    toggleActions: "restart reverse play reverse",
+  },
+  x: 200,
+  opacity: 0,
+  duration: 0.5,
+});
+
+gsap.from(".contact-title", {
+  scrollTrigger: {
+    trigger: ".contact-title",
+    toggleActions: "restart reverse play reverse",
+  },
+  y: -200,
+  opacity: 0,
+  duration: 0.5,
+});
+
+gsap.from(".contact-email", {
+  scrollTrigger: {
+    trigger: ".contact-title",
+    toggleActions: "restart reverse play reverse",
+  },
+  y: 200,
+  opacity: 0,
+  duration: 1,
+});
+
+gsap.from(".copy-clipboard", {
+  scrollTrigger: {
+    trigger: ".copy-clipboard",
+    toggleActions: "restart reverse play reverse",
+  },
+  y: 100,
+  opacity: 0,
+  duration: 1,
+});
+
+document.querySelector(".about-me-button").addEventListener("click", () => {
+  const homeContainer = document.querySelector(".home-container");
+  const aboutMeButton = document.querySelector(".about-me-button");
+
+  if (homeContainer.classList.contains("shifted")) {
+    homeContainer.classList.remove("shifted");
+    aboutMeButton.textContent = "about me ->";
+  } else {
+    homeContainer.classList.add("shifted");
+    aboutMeButton.textContent = "<- back";
+  }
+});
+
+const button = document.querySelector(".copy-clipboard");
+const tooltip = button.querySelector(".tooltip");
+
+button.addEventListener("click", function () {
+  const emailText = document.querySelector(".contact-email").textContent;
+
+  navigator.clipboard
+    .writeText(emailText)
+    .then(() => {
+      tooltip.textContent = "Copied to clipboard!";
+      setTimeout(() => {
+        tooltip.textContent = "Copy to clipboard";
+      }, 2000);
+    })
+    .catch((err) => console.error("Copy failed:", err));
+});
+
+button.addEventListener("mouseover", function () {
+  tooltip.textContent = "Copy to clipboard";
+});
